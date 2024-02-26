a9df0007622d0ace05da3b5a05df20c6f83cdeb5
Diogo Rocha <diogorocha222@sapo.pt>
2024-02-26 20:56:53 +0000

    fix css

--- updates/1.3.4.js
+++ updates/1.3.4.js
@@ -13,8 +13,23 @@ FAE.update_step = [
     info: 'Getting fa_advanced.min.css',
     type: 'GET',
     url: FAE.raw + 'css/fa_advanced.min.css',
-    func: function(d) {
-      FAE.step[FAE.index + 1].data.edit_code = d;
+    func: function (d) {
+      var cssCode = FAE.translate ? FAE.translate({
+        from: FAE.lang_current.css,
+        to: FAE.lang_new.css
+      }, d) : d;
+
+      // Atualizando o código CSS na próxima etapa
+      FAE.step[FAE.index + 1].data.edit_code = cssCode;
+    }
+  },
+  {
+    info: 'Installing fa_advanced.min.css',
+    type: 'POST',
+    url: 'part=themes&sub=logos&mode=css&extended_admin=1',
+    data: {
+      edit_code: '',
+      submit: 'Submit'
     }
   }
 ];
